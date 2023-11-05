33239e9033176793ec78300faed0188da8beb072
saipranay47 <saipranayadepu07@gmail.com>
2023-11-05 22:14:10 +0530

    update readme

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
+import { motion } from "framer-motion";
+
 function App() {
+  const buttonVariants = {
+    initial: {
+      scale: 1,
+    },
+    hover: {
+      scale: 1.1,
+    },
+  };
+
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello World</h1>
-    </>
+    <div className="flex justify-center items-center bg-black text-white min-h-screen font-hnd">
+      <motion.button
+        className="text-[80px] border-2 px-24 py-10 rounded-full"
+        variants={buttonVariants}
+        initial="initial"
+        whileHover="hover"
+      >
+        Get started&nbsp;→
+      </motion.button>
+    </div>
   );
 }
 
 export default App;
+  
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "hnd": ["Helvetica Now Display", "sans"],
+      },
+    },
   },
   plugins: [],
 };
